92fa2be0eb056da6e7011bb95de87bf25e1fe72a
Jenna Murrell <murrelljenna@gmail.com>
2023-04-28 22:46:02 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
